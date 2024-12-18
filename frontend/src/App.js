1704ed9fd6f0abae81f3b86316f24e37a9e01405
xzedm <abdueshim2004@gmail.com>
2024-12-18 21:59:03 +0500

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import './App.css';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import HomeScreen from './Screens/HomeScreen';
 import ProductScreen from './Screens/ProductScreen';
+import CartScreen from './Screens/CartScreen';
+import SigninScreen from './Screens/SigninScreen';
+import { useSelector } from 'react-redux';
+import RegisterScreen from './Screens/RegisterScreen';
+import ProductsScreen from './Screens/ProductsScreen';
+import ShippingScreen from './Screens/ShippingScreen';
+import PaymentScreen from './Screens/PaymentScreen';
+import PlaceOrderScreen from './Screens/PlaceOrderScreen';
+import OrderScreen from './Screens/OrderScreen';
+import ProfileScreen from './Screens/ProfileScreen';
+import OrdersScreen from './Screens/OrdersScreen';
 
 function App() {
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
 
   const openMenu = () => {
     document.querySelector('.sidebar').classList.add('open');
@@ -12,52 +25,70 @@ function App() {
   const closeMenu = () => {
     document.querySelector('.sidebar').classList.remove('open');
   };
-
   return (
-  <BrowserRouter>
-    <div className="grid-container">
-    <header className="header">
-      <div className="brand">
-        <button onClick={openMenu}>
-          &#9776;
-        </button>
-        <Link to="/">store</Link>
-      </div>
-      <div className="header-links">
-        <a href="cart.html">Cart</a>
-        <a href="signin.html">Sign In</a>
-      </div>
-    </header>
-    <aside className="sidebar">
-      <h3>Shopping Categories</h3>
-      <button className="sidebar-close-button" onClick={closeMenu}>x</button>
-      <ul>
-        <li>
-          <a href="index.html">Pants</a>
-        </li>
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="header">
+          <div className="brand">
+            <button onClick={openMenu}>&#9776;</button>
+            <Link to="/">store</Link>
+          </div>
+          <div className="header-links">
+            <a href="cart.html">Cart</a>
+            {userInfo ? (
+              <Link to="/profile">{userInfo.name}</Link>
+            ) : (
+              <Link to="/signin">Sign In</Link>
+            )}
+            {userInfo && userInfo.isAdmin && (
+              <div className="dropdown">
+                <a href="#">Admin</a>
+                <ul className="dropdown-content">
+                  <li>
+                    <Link to="/orders">Orders</Link>
+                    <Link to="/products">Products</Link>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
+        </header>
+        <aside className="sidebar">
+          <h3>Shopping Categories</h3>
+          <button className="sidebar-close-button" onClick={closeMenu}>
+            x
+          </button>
+          <ul className="categories">
+            <li>
+              <Link to="/category/Pants">Pants</Link>
+            </li>
 
-        <li>
-          <a href="index.html">Shirts</a>
-        </li>
-
-      </ul>
-    </aside>
-    <main className="main">
-      <div className="content">
-        <Routes>
-          <Route path="/products/:id" element={<ProductScreen />} />
-          <Route path="/" exact={true} element={<HomeScreen />} />
-        </Routes>
+            <li>
+              <Link to="/category/Shirts">Shirts</Link>
+            </li>
+          </ul>
+        </aside>
+        <main className="main">
+          <div className="content">
+            <Route path="/orders" element={<OrdersScreen />} />
+            <Route path="/profile" element={<ProfileScreen />} />
+            <Route path="/order/:id" element={<OrderScreen />} />
+            <Route path="/products" element={<ProductsScreen />} />
+            <Route path="/shipping" element={<ShippingScreen />} />
+            <Route path="/payment" element={<PaymentScreen />} />
+            <Route path="/placeorder" element={<PlaceOrderScreen />} />
+            <Route path="/signin" element={<SigninScreen />} />
+            <Route path="/register" element={<RegisterScreen />} />
+            <Route path="/product/:id" element={<ProductScreen />} />
+            <Route path="/cart/:id?" element={<CartScreen />} />
+            <Route path="/category/:id" element={<HomeScreen />} />
+            <Route path="/" exact={true} element={<HomeScreen />} />
+          </div>
+        </main>
+        <footer className="footer">All right reserved.</footer>
       </div>
-
-    </main>
-    <footer className="footer">
-      All right reserved.
-    </footer>
-  </div>
-  </BrowserRouter>
+    </BrowserRouter>
   );
 }
-
 
 export default App;
--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,40 +1,92 @@
-import React, { useEffect, useState } from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
+import { useSelector, useDispatch } from 'react-redux';
+import { listProducts } from '../actions/productActions';
+import Rating from '../components/Rating';
+import { useParams } from 'react-router-dom';
 
 function HomeScreen(props) {
+  const [searchKeyword, setSearchKeyword] = useState('');
+  const [sortOrder, setSortOrder] = useState('');
+  const { id } = useParams();
+  const category = id ? id : '';
+  const productList = useSelector((state) => state.productList);
+  const { products, loading, error } = productList;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(listProducts(category));
 
-    const [products, setProduct] = useState([]);
+    return () => {
+      //
+    };
+  }, [category]);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const { data } = await axios.get('/api/products');
-                setProduct(data);
-            } catch (error) {
-                console.error("Error fetching data:", error);
-            }
-        }
-        fetchData();
-    }, []);
-    
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(listProducts(category, searchKeyword, sortOrder));
+  };
+  const sortHandler = (e) => {
+    setSortOrder(e.target.value);
+    dispatch(listProducts(category, searchKeyword, sortOrder));
+  };
 
-    return <ul className="products">
-              { 
-              products.map(product => 
-              <li key={product._id}>
-                <div className="product">
+  return (
+    <>
+      {category && <h2>{category}</h2>}
+
+      <ul className="filter">
+        <li>
+          <form onSubmit={submitHandler}>
+            <input
+              name="searchKeyword"
+              onChange={(e) => setSearchKeyword(e.target.value)}
+            />
+            <button type="submit">Search</button>
+          </form>
+        </li>
+        <li>
+          Sort By{' '}
+          <select name="sortOrder" onChange={sortHandler}>
+            <option value="">Newest</option>
+            <option value="lowest">Lowest</option>
+            <option value="highest">Highest</option>
+          </select>
+        </li>
+      </ul>
+      {loading ? (
+        <div>Loading...</div>
+      ) : error ? (
+        <div>{error}</div>
+      ) : (
+        <ul className="products">
+          {products.map((product) => (
+            <li key={product._id}>
+              <div className="product">
                 <Link to={'/product/' + product._id}>
-                <img className="product-image" src="/images/nike.png" alt="product" />
+                  <img
+                    className="product-image"
+                    src={product.image}
+                    alt="product"
+                  />
                 </Link>
-                  <div className="product-name">
-                    <Link to={'/product/' + product._id}>{product.name}</Link>
-                  </div>
-                  <div className="product-brand">{product.brand}</div>
-                  <div className="product-price">${product.price}</div>
-                  <div className="product-rating">{product.rating} stars</div>
+                <div className="product-name">
+                  <Link to={'/product/' + product._id}>{product.name}</Link>
                 </div>
-              </li>)}
-            </ul>
+                <div className="product-brand">{product.brand}</div>
+                <div className="product-price">${product.price}</div>
+                <div className="product-rating">
+                  <Rating
+                    value={product.rating}
+                    text={product.numReviews + ' reviews'}
+                  />
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
 }
 export default HomeScreen;
